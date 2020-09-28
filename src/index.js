b8e45eef69dd4e043e2e371d88eb0d14694e3029
vseles <vitor_seles@outlook.com>
2020-09-28 18:03:41 -0300

    add fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import GlobalStyle from './styles/global';
+import './styles/_fonts.css';
 
 ReactDOM.render(
   <React.Fragment>
