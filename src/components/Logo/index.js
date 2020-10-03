0aa53e89f9eb6c880626487f75694c894d1947cc
vseles <vitor_seles@outlook.com>
2020-10-03 18:42:45 -0300

    change logo styles

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,10 +5,12 @@ import { Colors } from '../../styles/constants';
 
 const Logo = ({ color, size }) => {
   return (
-    <SC.Logo color={ color || Colors.SUB_COLOR }>
-      <RiMapPinFill color={ color || Colors.SUB_COLOR } size={ size || 32 } />
-      <SC.Title>PointCEP</SC.Title>
-    </SC.Logo>
+    <SC.Href href="/" title="PointCEP">
+      <SC.Logo color={ color || Colors.SUB_COLOR }>
+        <RiMapPinFill color={ color || Colors.SUB_COLOR } size={ size || 32 } />
+        <SC.Title>PointCEP</SC.Title>
+      </SC.Logo>
+    </SC.Href>
   );
 };
 
--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
 
+export const Href = styled.a`
+  text-decoration: none;
+`;
+
 export const Logo = styled.div`
   display: flex;
   align-items: center;
   color: ${ props => props.color };
 `;
 
-export const Title = styled.h1`
+export const Title = styled.em`
   margin-left: 6px;
-  font-size: 125%;
+  font-size: 150%;
   color: inherit;
+  opacity: 0.89;
+  font-style: normal;
+  font-weight: 700;
+  font-family: 'Metropolis Black';
 `;
